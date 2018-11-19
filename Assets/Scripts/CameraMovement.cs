b0482c5f06ced3ab4ce7c50fcbaf796e6025f7ac
lipuhossain <kutu1234>
2018-11-20 00:42:23 +0600

    Solved the Camera issue

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,7 +13,7 @@ public class CameraMovement : MonoBehaviour {
 	
 	void LateUpdate () {
 		if (SnakeContainer.childCount > 0) {
-			transform.position = Vector3.Slerp (transform.position,(initialCameraPos+new Vector3(0,SnakeContainer.GetChild(0).position.y - Camera.main.orthographicSize/2,0)),0.1f);
+			transform.position = Vector3.Slerp (transform.position,SnakeContainer.GetChild(1).position + new Vector3(0,2f,-10),0.1f);
 		}
 	}
 }
